641e88ac86092abaf64a06826a3916967bc7309f
Cahuete0512 <74617561+Cahuete0512@users.noreply.github.com>
2022-06-22 19:08:24 +0200

    conf pour communication avec API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EvenementComponent } from './evenement/evenement.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EvenementComponent
   ],
   imports: [
     BrowserModule
